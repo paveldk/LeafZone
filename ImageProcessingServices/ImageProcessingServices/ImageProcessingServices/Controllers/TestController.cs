9c2f9ed8deb5e65fbea9380d90e27d824f10c3d7
paveldk <i.pavelkolev@gmail.com>
2014-04-12 13:00:03 +0300

    Services core

---
+++ ImageProcessingServices/ImageProcessingServices/ImageProcessingServices/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ImageProcessingServices.Controllers
+{
+    public class TestController : ApiController
+    {
+        // GET api/test/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/test
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/test/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/test/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ImageProcessingServices/ImageProcessingServices/ImageProcessingServices/Models/AnalyzeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImageProcessingServices.Models
+{
+    public class AnalyzeModel
+    {
+        public string ImageName { get; set; }
+        public string ImageBase64 { get; set; }
+        
+        public AnalyzeModel() 
+        {
+
+        }
+    }
+}
